1da608887d34a14a99f5ce01bc3cae630355d2eb
aidera <hope.aidera@gmail.com>
2020-08-01 09:27:07 +0300

    Changed config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-  port: 5000,
+  port: process.env.NODE_ENV === "production" ? 80 : 5000,
   baseUrl: "http://localhost:5000",
   jwtSecret: "the cat walks trough my errors",
   mongoUri: "mongodb+srv://aidera:password1010@cluster0-r7sxs.mongodb.net/app?retryWrites=true",
